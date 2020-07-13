709765cd36914417b6cb6bab4aef61df42225688
Gustavo Ferraresi <gustaferraresi189@gmail.com>
2020-07-13 01:48:57 -0300

    Created the class "Polynomial", as well as the files "Constants.hpp" and "Enum_Types.hpp"

---
+++ Enum_Types.hpp
@@ -0,0 +1,20 @@
+#ifndef LAB_6_ENUM_TYPES
+#define LAB_6_ENUM_TYPES
+
+// Polynomial type
+enum Type
+{
+    TYPE_INPUT,
+    TYPE_OUTPUT,
+};
+
+// Operations
+enum Operation
+{
+    OP_SUM,
+    OP_SUB,
+    OP_MUL,
+    OP_DIV,
+};
+
+#endif // LAB_6_ENUM_TYPES
---
+++ Polynomial.hpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+#include "Constants.hpp"
+#include "Enum_Types.hpp"
+
+using namespace std;
+
+# ifndef LAB_6_POLYNOMIAL
+# define LAB_6_POLYNOMIAL
+
+class Polynomial
+{
+    private:
+        vector<int> coeff;
+        Type type;
+};
+
+#endif // LAB_6_POLYNOMIAL
